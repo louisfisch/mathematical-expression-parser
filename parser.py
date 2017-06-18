c9ac2e3594fab83c640ca310d86c0977ecb6b7bc
Louis Fischer <louis.fischer@me.com>
2017-06-18 20:16:25 +0200

    Isolate Parser class. Update .gitignore and README

--- parser.py
+++ parser.py
@@ -1,6 +1,5 @@
 import math
 
-
 _CONSTANTS = {
     'pi' : math.pi,
     'e' : math.e,
@@ -213,34 +212,3 @@ class Parser:
                     "I was expecting to find a number at character " + str(self.index) + " but instead I found a '" + char + "'. What's up with that?")
 
         return float(strValue)
-
-def evaluate(expression, vars = None):
-    try:
-        p = Parser(expression, vars)
-        value = p.getValue()
-    except Exception as (ex):
-        msg = ex.message
-        raise Exception(msg)
-
-    # Return an integer type if the answer is an integer 
-    if int(value) == value:
-        return int(value)
-
-    # If Python made some silly precision error like x.99999999999996, just return x+1 as an integer 
-    epsilon = 0.0000000001
-    if int(value + epsilon) != int(value):
-        return int(value + epsilon)
-    if int(value - epsilon) != int(value):
-        return int(value)
-    return value
-
-if __name__ == "__main__":
-    print evaluate("cos(x+4*3) + 2 * 3", { 'x': 5  })
-    print evaluate("exp(0)")
-    print evaluate("-(1 + 2) * 3")
-    print evaluate("(1-2)/3.0 + 0.0000")
-    print evaluate("abs(-2) + pi / 4")
-    print evaluate("(x + e * 10) / 10", { 'x' : 3 })
-    print evaluate("1.0 / 3 * 6")
-    print evaluate("(1 - 1 + -1) * pi")
-    print evaluate("cos(pi) * 1")
